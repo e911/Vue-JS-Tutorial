58aa05771f4d3ee19ec9ef02071ccd60da5f84b3
t_barun <pujanthapa88.63@gmail.com>
2018-10-08 15:09:53 +0545

    Vue js tutorial commence

---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  // components: { App },
+  // template: '<App/>'
+  data: {
+    name: 'Pujan',
+    job: 'Ninja',
+    website: 'http://thapapujan.com.np',
+    websiteTag: '<a href="http://thapapujan.com.np">Site</a>'
+  },
+  methods: {
+    greet: function (time) {
+      return 'Good ' + time + ' ' + this.name
+    }
+  }
+})
